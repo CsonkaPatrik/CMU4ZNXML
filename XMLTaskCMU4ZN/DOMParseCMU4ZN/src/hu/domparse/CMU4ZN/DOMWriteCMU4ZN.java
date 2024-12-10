26e536eafa0ba213d40d39781544ee08fe088f9e
CsonkaPatrik <99277973+CsonkaPatrik@users.noreply.github.com>
2024-12-10 17:17:04 +0100

    fixed the output file

--- XMLTaskCMU4ZN/DOMParseCMU4ZN/src/hu/domparse/CMU4ZN/DOMWriteCMU4ZN.java
+++ XMLTaskCMU4ZN/DOMParseCMU4ZN/src/hu/domparse/CMU4ZN/DOMWriteCMU4ZN.java
@@ -3,6 +3,10 @@ package hu.domparse.CMU4ZN;
 import org.w3c.dom.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 
 public class DOMWriteCMU4ZN {
     public static void main(String[] args) {
@@ -12,12 +16,17 @@ public class DOMWriteCMU4ZN {
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document doc = builder.parse("XMLCMU4ZN.xml");
 
+            // Dokumentum normalizálása
             doc.getDocumentElement().normalize();
 
-            // Gyökérelem kiírása
+            // Gyökérelem és XML fa kiírása a konzolra
             System.out.println("XML tree structure:");
             printElement(doc.getDocumentElement(), 0);
 
+            // XML dokumentum mentése új fájlba
+            writeDocumentToFile(doc, "XMLCMU4ZN_Output.xml");
+
+            System.out.println("The XML content has been successfully written to XMLCMU4ZN_Output.xml.");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -27,11 +36,9 @@ public class DOMWriteCMU4ZN {
     private static void printElement(Element element, int indent) {
         // Behúzás az aktuális elem szintjének megfelelően
         for (int i = 0; i < indent; i++) System.out.print("  ");
-        
-        // Elem neve
+
+        // Elem neve és attribútumai
         System.out.print("<" + element.getTagName());
-        
-        // Attribútumok kiírása
         NamedNodeMap attributes = element.getAttributes();
         for (int i = 0; i < attributes.getLength(); i++) {
             Node attribute = attributes.item(i);
@@ -43,11 +50,9 @@ public class DOMWriteCMU4ZN {
         NodeList children = element.getChildNodes();
         for (int i = 0; i < children.getLength(); i++) {
             Node node = children.item(i);
-
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 printElement((Element) node, indent + 1);
             } else if (node.getNodeType() == Node.TEXT_NODE) {
-                // Szöveges tartalom kiírása (ha nem üres)
                 String textContent = node.getTextContent().trim();
                 if (!textContent.isEmpty()) {
                     for (int j = 0; j <= indent; j++) System.out.print("  ");
@@ -56,8 +61,22 @@ public class DOMWriteCMU4ZN {
             }
         }
 
-        // Záró elem kiírása
+        // Záró elem
         for (int i = 0; i < indent; i++) System.out.print("  ");
         System.out.println("</" + element.getTagName() + ">");
+    }
+
+    // Metódus az XML dokumentum fájlba írására
+    private static void writeDocumentToFile(Document doc, String filename) {
+        try {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(filename));
+            transformer.transform(source, result);
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
     }
 }
